c07e6c8f26ad6ecc52eed6467986912ad0c5c9c4
Md. Kamrul Hasan Tanbir <159300065+kamrulhasantanbir@users.noreply.github.com>
2025-01-27 18:53:49 +0600

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+      int a = 100;
+      long b = 99999999999L;
+      double d = 843.6348;
+      char c = 't';
+      String s = "Tanbir";
+      System.out.println(a);
+      System.out.print(c + "\n");
+      System.out.println(b);
+      System.out.print("My name is : " + s + "\n");
+      System.out.print("My montly income is :" + d + "$");
+
+    }
+}
